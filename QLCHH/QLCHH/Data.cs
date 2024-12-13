7b20708f6bb675cad3b228f0e9c3af00cf3af74d
QuynhChuong <quynhchuongtran@gmail.com>
2024-12-13 16:20:18 +0700

    them class data

---
+++ QLCHH/QLCHH/Data.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace QLCHH
+{
+    class Data
+    {
+        SqlConnection cn;
+        public Data(string srvname, string dbname)
+        {
+            cn = new SqlConnection(@"Data Source=" + srvname + ";Initial Catalog=" + dbname + ";Integrated Security=True");
+        }
+        public DataTable laydl(string sql)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+            da.Fill(dt);
+            return dt;
+        }
+        public void thucthi(string sql)
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+    }
+}
