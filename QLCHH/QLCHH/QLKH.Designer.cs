7f563b59a679066557c2fd8a0ab5d83138f78b1a
Baitaplon <nhu022101052@tgu.edu.vn>
2024-12-14 16:11:47 +0700

    Add lại

---
+++ QLCHH/QLCHH/QLKH.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace QLCHH
+{
+    partial class QLKH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtmakh = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txthoten = new System.Windows.Forms.TextBox();
+            this.txtdiachi = new System.Windows.Forms.TextBox();
+            this.txtsdt = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.btthem = new System.Windows.Forms.Button();
+            this.btcapnhat = new System.Windows.Forms.Button();
+            this.btluu = new System.Windows.Forms.Button();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.btthoat = new System.Windows.Forms.Button();
+            this.bttimkiem = new System.Windows.Forms.Button();
+            this.txttimkiem = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtgvdanhsach = new System.Windows.Forms.DataGridView();
+            this.rdmakh = new System.Windows.Forms.RadioButton();
+            this.rdhoten = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvdanhsach)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtmakh
+            // 
+            this.txtmakh.Location = new System.Drawing.Point(197, 43);
+            this.txtmakh.Multiline = true;
+            this.txtmakh.Name = "txtmakh";
+            this.txtmakh.Size = new System.Drawing.Size(253, 34);
+            this.txtmakh.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(498, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btthoat);
+            this.groupBox1.Controls.Add(this.btxoa);
+            this.groupBox1.Controls.Add(this.btluu);
+            this.groupBox1.Controls.Add(this.btcapnhat);
+            this.groupBox1.Controls.Add(this.btthem);
+            this.groupBox1.Controls.Add(this.txtemail);
+            this.groupBox1.Controls.Add(this.txtsdt);
+            this.groupBox1.Controls.Add(this.txtdiachi);
+            this.groupBox1.Controls.Add(this.txthoten);
+            this.groupBox1.Controls.Add(this.txtmakh);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(32, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(484, 493);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdhoten);
+            this.groupBox2.Controls.Add(this.rdmakh);
+            this.groupBox2.Controls.Add(this.txttimkiem);
+            this.groupBox2.Controls.Add(this.bttimkiem);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(538, 80);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(793, 125);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tìm kiếm";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dtgvdanhsach);
+            this.groupBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(538, 211);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(793, 362);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Danh sách khách hàng:";
+            // 
+            // txthoten
+            // 
+            this.txthoten.Location = new System.Drawing.Point(197, 116);
+            this.txthoten.Multiline = true;
+            this.txthoten.Name = "txthoten";
+            this.txthoten.Size = new System.Drawing.Size(253, 34);
+            this.txthoten.TabIndex = 1;
+            // 
+            // txtdiachi
+            // 
+            this.txtdiachi.Location = new System.Drawing.Point(197, 190);
+            this.txtdiachi.Multiline = true;
+            this.txtdiachi.Name = "txtdiachi";
+            this.txtdiachi.Size = new System.Drawing.Size(253, 34);
+            this.txtdiachi.TabIndex = 2;
+            // 
+            // txtsdt
+            // 
+            this.txtsdt.Location = new System.Drawing.Point(197, 255);
+            this.txtsdt.Multiline = true;
+            this.txtsdt.Name = "txtsdt";
+            this.txtsdt.Size = new System.Drawing.Size(253, 34);
+            this.txtsdt.TabIndex = 3;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(197, 325);
+            this.txtemail.Multiline = true;
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(253, 34);
+            this.txtemail.TabIndex = 4;
+            // 
+            // btthem
+            // 
+            this.btthem.Location = new System.Drawing.Point(33, 387);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(109, 33);
+            this.btthem.TabIndex = 0;
+            this.btthem.Text = "Thêm";
+            this.btthem.UseVisualStyleBackColor = true;
+            this.btthem.Click += new System.EventHandler(this.btthem_Click);
+            // 
+            // btcapnhat
+            // 
+            this.btcapnhat.Location = new System.Drawing.Point(176, 387);
+            this.btcapnhat.Name = "btcapnhat";
+            this.btcapnhat.Size = new System.Drawing.Size(109, 33);
+            this.btcapnhat.TabIndex = 5;
+            this.btcapnhat.Text = "Cập nhật";
+            this.btcapnhat.UseVisualStyleBackColor = true;
+            this.btcapnhat.Click += new System.EventHandler(this.btcapnhat_Click);
+            // 
+            // btluu
+            // 
+            this.btluu.Location = new System.Drawing.Point(312, 387);
+            this.btluu.Name = "btluu";
+            this.btluu.Size = new System.Drawing.Size(109, 33);
+            this.btluu.TabIndex = 6;
+            this.btluu.Text = "Lưu";
+            this.btluu.UseVisualStyleBackColor = true;
+            this.btluu.Click += new System.EventHandler(this.btluu_Click);
+            // 
+            // btxoa
+            // 
+            this.btxoa.Location = new System.Drawing.Point(100, 438);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(109, 33);
+            this.btxoa.TabIndex = 7;
+            this.btxoa.Text = "Xóa";
+            this.btxoa.UseVisualStyleBackColor = true;
+            this.btxoa.Click += new System.EventHandler(this.btxoa_Click);
+            // 
+            // btthoat
+            // 
+            this.btthoat.Location = new System.Drawing.Point(255, 438);
+            this.btthoat.Name = "btthoat";
+            this.btthoat.Size = new System.Drawing.Size(109, 33);
+            this.btthoat.TabIndex = 8;
+            this.btthoat.Text = "Thoát";
+            this.btthoat.UseVisualStyleBackColor = true;
+            this.btthoat.Click += new System.EventHandler(this.btthoat_Click);
+            // 
+            // bttimkiem
+            // 
+            this.bttimkiem.Location = new System.Drawing.Point(555, 51);
+            this.bttimkiem.Name = "bttimkiem";
+            this.bttimkiem.Size = new System.Drawing.Size(137, 33);
+            this.bttimkiem.TabIndex = 1;
+            this.bttimkiem.Text = "Tìm kiếm";
+            this.bttimkiem.UseVisualStyleBackColor = true;
+            this.bttimkiem.Click += new System.EventHandler(this.bttimkiem_Click);
+            // 
+            // txttimkiem
+            // 
+            this.txttimkiem.Location = new System.Drawing.Point(150, 50);
+            this.txttimkiem.Multiline = true;
+            this.txttimkiem.Name = "txttimkiem";
+            this.txttimkiem.Size = new System.Drawing.Size(361, 34);
+            this.txttimkiem.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 27);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Mã KH:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 27);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Họ tên:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 27);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Địa chỉ:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 262);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 27);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "SĐT:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 332);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 27);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Email:";
+            // 
+            // dtgvdanhsach
+            // 
+            this.dtgvdanhsach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvdanhsach.Location = new System.Drawing.Point(32, 35);
+            this.dtgvdanhsach.Name = "dtgvdanhsach";
+            this.dtgvdanhsach.RowTemplate.Height = 28;
+            this.dtgvdanhsach.Size = new System.Drawing.Size(736, 305);
+            this.dtgvdanhsach.TabIndex = 0;
+            this.dtgvdanhsach.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvdanhsach_CellClick);
+            // 
+            // rdmakh
+            // 
+            this.rdmakh.AutoSize = true;
+            this.rdmakh.Location = new System.Drawing.Point(23, 35);
+            this.rdmakh.Name = "rdmakh";
+            this.rdmakh.Size = new System.Drawing.Size(109, 31);
+            this.rdmakh.TabIndex = 4;
+            this.rdmakh.TabStop = true;
+            this.rdmakh.Text = "Mã KH";
+            this.rdmakh.UseVisualStyleBackColor = true;
+            // 
+            // rdhoten
+            // 
+            this.rdhoten.AutoSize = true;
+            this.rdhoten.Location = new System.Drawing.Point(23, 73);
+            this.rdhoten.Name = "rdhoten";
+            this.rdhoten.Size = new System.Drawing.Size(102, 31);
+            this.rdhoten.TabIndex = 5;
+            this.rdhoten.TabStop = true;
+            this.rdhoten.Text = "Họ tên";
+            this.rdhoten.UseVisualStyleBackColor = true;
+            // 
+            // QLKH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1376, 638);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "QLKH";
+            this.Text = "QLKH";
+            this.Load += new System.EventHandler(this.QLKH_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvdanhsach)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtmakh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtsdt;
+        private System.Windows.Forms.TextBox txtdiachi;
+        private System.Windows.Forms.TextBox txthoten;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btthoat;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.Button btluu;
+        private System.Windows.Forms.Button btcapnhat;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.RadioButton rdhoten;
+        private System.Windows.Forms.RadioButton rdmakh;
+        private System.Windows.Forms.TextBox txttimkiem;
+        private System.Windows.Forms.Button bttimkiem;
+        private System.Windows.Forms.DataGridView dtgvdanhsach;
+    }
+}
---
+++ QLCHH/QLCHH/QLKH.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCHH
+{
+    public partial class QLKH : Form
+    {
+        public QLKH()
+        {
+            InitializeComponent();
+        }
+
+        private void QLKH_Load(object sender, EventArgs e)
+        {
+            Ketnoi kn = new Ketnoi();
+            DataTable db = kn.laydl("SELECT * FROM QLKH");
+            dtgvdanhsach.DataSource = db;
+        }
+
+        private void dtgvdanhsach_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dtgvdanhsach.SelectedRows.Count > 0)
+            {
+                string makh = dtgvdanhsach.SelectedRows[0].Cells[0].Value.ToString();
+                txtmakh.Text = makh;
+                txthoten.Text = dtgvdanhsach.SelectedRows[0].Cells["HoTen"].Value.ToString();
+                txtdiachi.Text = dtgvdanhsach.SelectedRows[0].Cells["DiaChi"].Value.ToString();
+                txtsdt.Text = dtgvdanhsach.SelectedRows[0].Cells["SoDienThoai"].Value.ToString();
+                txtemail.Text = dtgvdanhsach.SelectedRows[0].Cells["Email"].Value.ToString();
+            }
+        }
+
+        private void btthem_Click(object sender, EventArgs e)
+        {
+            txtmakh.Clear();
+            txthoten.Clear();
+            txtdiachi.Clear();
+            txtsdt.Clear();
+            txtemail.Clear();
+
+            Ketnoi kn = new Ketnoi();
+            string masach = kn.laydl("SELECT TOP 1 Makhachhang FROM QLKH ORDER BY Makhachhang DESC").Rows[0]["Makhachhang"].ToString();
+            txtmakh.Text = "KH" + (int.Parse(masach.Substring(2)) + 1).ToString("D3");
+        }
+
+        private void btcapnhat_Click(object sender, EventArgs e)
+        {
+            if (txtmakh.Text == "" || txthoten.Text == "" || txtdiachi.Text == "" || txtsdt.Text == "" ||
+            txtemail.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ tất cả thông tin!");
+                return;
+            }
+
+            DataGridViewRow dongdachon = dtgvdanhsach.SelectedRows[0];
+
+            dongdachon.Cells["Makhachhang"].Value = txtmakh.Text;
+            dongdachon.Cells["HoTen"].Value = txthoten.Text;
+            dongdachon.Cells["DiaChi"].Value = txtdiachi.Text;
+            dongdachon.Cells["SoDienThoai"].Value = txtsdt.Text;
+            dongdachon.Cells["Email"].Value = txtemail.Text;
+
+            MessageBox.Show("Cập nhật sách thành công!");
+
+            txtmakh.Clear();
+            txthoten.Clear();
+            txtdiachi.Clear();
+            txtsdt.Clear();
+            txtemail.Clear();
+        }
+
+        private void btluu_Click(object sender, EventArgs e)
+        {
+            if (txtmakh.Text == "" || txthoten.Text == "" || txtdiachi.Text == "" || txtsdt.Text == "" ||
+            txtemail.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ tất cả thông tin!");
+                return;
+            }
+            Ketnoi kn = new Ketnoi();
+            kn.thucthi($"INSERT INTO QLKH (Makhachhang, HoTen, DiaChi, SoDienThoai, Email) VALUES " +
+                   $"('{txtmakh.Text}', N'{txthoten.Text}', N'{txtdiachi.Text}', '{txtsdt.Text}', '{txtemail.Text}')");
+
+            // Cập nhật lại DataGridView
+            dtgvdanhsach.DataSource = kn.laydl("SELECT * FROM QLKH");
+            MessageBox.Show("Lưu thông tin thành công!");
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            string makh = dtgvdanhsach.SelectedRows[0].Cells["Makhachhang"].Value.ToString();
+            if (MessageBox.Show("Bạn muốn xóa khách hàng  không ?", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Ketnoi kn = new Ketnoi();
+                string sql = $"DELETE FROM QLKH WHERE Makhachhang = '{makh}'";
+                kn.laydl(sql);
+
+
+                DataTable db = kn.laydl("SELECT * FROM QLKH");
+                dtgvdanhsach.DataSource = db;
+            }
+        }
+
+        private void btthoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn đóng giao diện?", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Hide();
+
+                this.Close();
+            }
+        }
+
+        private void bttimkiem_Click(object sender, EventArgs e)
+        {
+            Ketnoi kn = new Ketnoi();
+            string sql = "";
+
+            string searchText = txttimkiem.Text.Trim();
+
+
+            if (rdmakh.Checked)
+            {
+                sql = "SELECT * FROM QLKH WHERE Makhachhang LIKE '%" + txttimkiem.Text + "%'";
+            }
+            else
+            {
+                sql = "SELECT * FROM QLKH WHERE HoTen COLLATE Vietnamese_CI_AI LIKE '%" + txttimkiem.Text + "%'";
+            }
+
+            DataTable db = kn.laydl(sql);
+            dtgvdanhsach.DataSource = db;
+            txttimkiem.Clear();
+        }
+    }
+}
